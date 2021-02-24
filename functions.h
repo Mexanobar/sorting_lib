d94ee820b7e94ed235bc69ccfff4b7bf334e09fe
Mexanobar <mexanobar@gmail.com>
2021-02-24 22:48:49 +0300

    first commit

---
+++ functions.h
@@ -0,0 +1,7 @@
+#include <vector>
+using std::vector;
+void bubble(vector<int> &arr);
+void selection(vector<int> &arr);
+void insertion(vector<int> &arr);
+void qsort(vector<int> &arr, int beg, int end);
+void mergeSort(vector<int> &arr);
